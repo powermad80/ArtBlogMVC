b68e8d7b096f0d78df32abe2c08a6fd16c0c0b95
Jake Dubusker <jakedubusker@gmail.com>
2018-01-18 20:56:49 -0500

    no message

--- ArtBlogMVC/wwwroot/js/index.js
+++ ArtBlogMVC/wwwroot/js/index.js
@@ -7,7 +7,32 @@
 }
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: props.title,
+            imgUrl: props.imgUrl,
+            description: props.description,
+            tags: props.tags
+        };
+    }
+    render() {
+        return (
+            <div className="post">
+                <div className="post-header">
+                    <h6>{this.state.title}</h6>
+                </div>
+                
+                <div className="post-img">
+                    <img src={this.state.imgUrl} height="1000" width="600" />
+                </div>
 
+                <div className="post-footer">
+                    <p>{this.props.description}</p>
+                </div>
+            </div>
+            )
+    }
 }
 
 
